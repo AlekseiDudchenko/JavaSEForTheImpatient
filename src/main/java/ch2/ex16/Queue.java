50fe5107adc7bf8148fb0d794c492f3601853b04
Aleksie Dudchenko <alexey.dudchenko@gmail.com>
2019-09-28 16:33:45 +0200

    Queue add, remove, tests

---
+++ src/main/java/ch2/ex16/Queue.java
@@ -0,0 +1,44 @@
+package ch2.ex16;
+
+import java.util.ArrayList;
+
+public class Queue {
+
+    /**
+     * Add element to the tail of the Queue
+     */
+    void addNode(String string){
+        Node newNode = new Node(string);
+        nodes.add(newNode);
+    }
+
+    /**
+     * Delete element from the top of the Queue
+     */
+    void  removeNode(){
+        nodes.remove(0);
+    }
+
+    public static class Node{
+        String string;
+
+        Node(String string){
+            this.string = string;
+        }
+
+        @Override
+        public String toString() {
+            return string;
+        }
+    }
+
+    private  ArrayList<Node> nodes = new ArrayList<>();
+
+    public ArrayList<Node> getNodes() {
+        return nodes;
+    }
+
+    public String toString(int nodeIndex) {
+        return nodes.get(nodeIndex).toString();
+    }
+}
