b87432648e1c6f264d321b4f44ff35ee0fc35cda
Aleksie Dudchenko <alexey.dudchenko@gmail.com>
2019-09-28 16:33:45 +0200

    Queue add, remove, tests

---
+++ src/test/java/ch2/ex16/QueueTest.java
@@ -0,0 +1,40 @@
+package ch2.ex16;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class QueueTest {
+
+    String str1 = "First string";
+    String str2 = "Second string";
+    String str3 = "Third string";
+
+
+    @Test
+    void addNodeTest() {
+        Queue queue = new Queue();
+        assertTrue(queue.getNodes().isEmpty());
+        queue.addNode(str1);
+        queue.addNode(str2);
+        assertEquals(str1, queue.toString(0));
+        assertEquals(str2, queue.toString(1));
+    }
+
+    @Test
+    void removeNodeTest(){
+        Queue queue = new Queue();
+        queue.addNode(str1);
+        queue.addNode(str2);
+        queue.addNode(str3);
+        assertEquals(str3, queue.toString(2));
+        assertEquals( 3, queue.getNodes().size());
+        queue.removeNode();
+        assertEquals( 2, queue.getNodes().size());
+        assertEquals(str2, queue.toString(0));
+        assertEquals(str3, queue.toString(1));
+    }
+
+
+}
