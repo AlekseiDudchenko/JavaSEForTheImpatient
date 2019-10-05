3494e1df06e738c45426ab1e25bf977e5398175a
Aleksie Dudchenko <alexey.dudchenko@gmail.com>
2019-10-05 22:33:53 +0200

    init commit for ch3 ex6. interface Sequence.

---
+++ src/main/java/ch3/ex6/Sequence.java
@@ -0,0 +1,11 @@
+package ch3.ex6;
+
+/**
+ *The SquareSequence class doesn’t actually deliver an infinite sequence of
+ * squares due to integer overflow. Specifically, how does it behave? Fix the
+ * problem by defining a Sequence<T> interface and a SquareSequence class that
+ * implements Sequence<BigInteger>.
+ */
+
+public interface Sequence {
+}
