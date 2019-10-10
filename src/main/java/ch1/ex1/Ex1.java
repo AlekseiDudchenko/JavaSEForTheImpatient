58af383a1d418fd44befc9f5695126ca34a6bad2
Aleksie Dudchenko <alexey.dudchenko@gmail.com>
2019-10-10 22:18:31 +0200

    Ex1 Calendar

---
+++ src/main/java/ch1/ex1/Ex1.java
@@ -0,0 +1,35 @@
+package ch1.ex1;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+/**
+ * Change the calendar printing program so it starts the week on a Sunday. Also make it print a newline at the end (but
+ * only one).
+ */
+
+public class Ex1 {
+
+    public static void main(String[] args) {
+
+        LocalDate currentDate = LocalDate.now().withDayOfMonth(1);
+        int month = currentDate.getMonthValue();
+
+        System.out.println(" Sun Mon Tue Wed Thu Fri Sat");
+
+        for (int i = 0; i < getDayOfWeekNewValue(currentDate.getDayOfWeek()) - 1; i++)
+            System.out.print("    ");
+
+        while (currentDate.getMonthValue() == month) {
+            System.out.printf("%4d", currentDate.getDayOfMonth());
+            currentDate = currentDate.plusDays(1);
+
+            if (getDayOfWeekNewValue(currentDate.getDayOfWeek()) == 1)
+                System.out.println();
+        }
+    }
+
+    private static int getDayOfWeekNewValue(DayOfWeek day) {
+        return (day.getValue() % 7) + 1;
+    }
+}
